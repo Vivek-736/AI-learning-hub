589c6b26ba2ac4c4d5e2553856088f0d99002ccc
Vivek <vivekousik8555@gmail.com>
2025-03-03 19:15:29 +0530

    irrevocable tweak to integrate vercel to inngest for deployment

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,8 +2,13 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { CreateNewUser, GenerateNotes, GenerateStudyContent, helloWorld } from "@/inngest/functions";
 
+export const runtime = 'edge';
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  
+  streaming: 'allow',
+
   functions: [
     helloWorld,
     CreateNewUser,
