fe7076bf5a16dcce1455a2bff6701f98f2b3877d
Vivek <vivekousik8555@gmail.com>
2025-03-03 20:23:39 +0530

    minuscule tweak to adjust the inngest function to work fine

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,12 +2,8 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { CreateNewUser, GenerateNotes, GenerateStudyContent, helloWorld } from "@/inngest/functions";
 
-export const runtime = 'edge';
-
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  
-  streaming: 'allow',
 
   functions: [
     helloWorld,
